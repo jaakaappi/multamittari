3922bc66860b102563818bbbdd1d5f49247bc42b
Esa Niemi <diapaasi@gmail.com>
2021-06-02 01:09:30 +0300

    Working soil moisture, air temp+hum

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
+#include "DHT.h"
 
 #include "config.h"
 
+#define DHTPIN 23
+#define DHTTYPE DHT22
+
+#define SOIL_MOISTURE_PIN 36
+#define ANALOG_RESOLUTION 4096.0
+#define WET_SOIL 1265
+#define DRY_SOIL 3838
+
+DHT dht(DHTPIN, DHTTYPE);
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
@@ -29,13 +39,18 @@ void setup_wifi()
   Serial.println(WiFi.localIP());
 }
 
-void setup_mqtt() {
-  while (!mqttClient.connected()) {
+void setup_mqtt()
+{
+  while (!mqttClient.connected())
+  {
     Serial.print("Attempting MQTT connection...");
-    if (mqttClient.connect("multamittari", access_token, "")) {
+    if (mqttClient.connect("multamittari", access_token, ""))
+    {
       Serial.println("connected");
       mqttClient.subscribe("esp32/output");
-    } else {
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(mqttClient.state());
       Serial.println(" try again in 5 seconds");
@@ -50,10 +65,42 @@ void setup()
   setup_wifi();
   mqttClient.setServer(mqtt_server, 1883);
   setup_mqtt();
+  dht.begin();
+}
+
+void readDht()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature();
+
+  if (isnan(h) || isnan(t) || isnan(f))
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+}
+
+void readSoilMoisture()
+{
+  // https://makersportal.com/blog/2020/5/26/capacitive-soil-moisture-calibration-with-arduino manual coefficients
+  float voltage = (float(analogRead(SOIL_MOISTURE_PIN)) / 4095.0) * 3.3;
+  float moisture = (1.0 / voltage) * 1.50 - 0.54;
+  Serial.print(F("Soil moisture "));
+  Serial.print(moisture);
+  Serial.println(F("cm³/cm³"));
 }
 
 void loop()
 {
-  mqttClient.publish("v1/devices/me/telemetry","{\"humidity\":0}");
+  mqttClient.publish("v1/devices/me/telemetry", "{\"humidity\":0}");
+  readDht();
+  readSoilMoisture();
   delay(5000);
 }
