d4c539e7c323403ca889946dd1d2c5caf01d752f
Esa Niemi <diapaasi@gmail.com>
2021-06-01 22:50:01 +0300

    Initial version

---
+++ src/config.h
@@ -0,0 +1,4 @@
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* mqtt_server = "YOUR_MQTT_BROKER_IP_ADDRESS";
+const char* access_token = "YOUR_THINGSBOARD_ACCESS_TOKEN";
